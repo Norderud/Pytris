05a542b12903f6b86d126c6c5cd6affd5f91820a
Åsmund Norderud <asmundno@live.no>
2019-12-23 12:58:27 +0100

    movement + grid class

--- pytris.py
+++ pytris.py
@@ -1,44 +1,48 @@
 import pygame
 import pygame.locals
 
-
 class Square:
-    def __init__(self, subsurface):
+    def __init__(self, rect):
         self.is_active = False
-        self.subsurface = subsurface
+        self.rect = rect
 
 
-def load_tile_table(filename, width, height):
-    image = pygame.image.load(filename).convert()
-    image_width, image_height = image.get_size()
-    tile_table = []
-    for tile_x in range(0, width):
-        line = []
-        tile_table.append(line)
-        for tile_y in range(0, height):
-            rect = (tile_x, tile_y, 30, 30)
-            line.append(Square(image.subsurface(rect)))
-    return tile_table
+class Grid:
+    def load_grid(self, width, height):
+        table = []
+        for tile_x in range(0, width):
+            line = []
+            table.append(line)
+            for tile_y in range(0, height):
+                rect = (tile_x*30, tile_y*30, 30, 30)
+                line.append(Square(rect))
+        return table
+
+    def __init__(self,width, height):
+        self.table = self.load_grid(width, height)
+
+
 
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((1280, 980))
-    table = load_tile_table("ground.png", 10, 20)
 
-    table[5][10].is_active = True
+    grid = Grid(10, 20)
 
-    pygame.display.flip()
+    grid.table[5][10].is_active = True
+    count = 0
+    
     while pygame.event.wait().type != pygame.locals.QUIT:
         screen.fill((255, 255, 255))
-
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
-            table[1][1].is_active = not table[1][1].is_active
-            
-        for x, row in enumerate(table):
+            grid.table[0][count].is_active = not grid.table[1][count].is_active
+            count +=1
+
+        for x, row in enumerate(grid.table):
             for y, square in enumerate(row):
                 if square.is_active:
-                    screen.blit(square.subsurface, (x*30, y*30))
+                    pygame.draw.rect(screen, (0, 0, 0), square.rect)
         pygame.display.update()
