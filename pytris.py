6c3f00019ac469ace0b15a674629e10551c16d21
Åsmund Norderud <asmundno@live.no>
2019-12-24 11:55:51 +0100

    ny blokk når en treffer bakken

--- pytris.py
+++ pytris.py
@@ -1,50 +1,5 @@
 import pygame
-
-
-class Square:
-    def __init__(self, rect):
-        self.is_active = False
-        self.rect = rect
-
-class Grid:
-    def load_grid(self, width, height):     
-        table = []
-        for x in range(0, width):
-            line = []
-            table.append(line)
-            for y in range(0, height):
-                rect = (x*30, y*30, 30, 30)
-                line.append(Square(rect))
-        return table
-
-    def __init__(self, width, height):
-        self.table = self.load_grid(width, height)
-        self.x_pos = 0
-        self.y_pos = 0
-
-    def move_right(self):
-        if self.x_pos == 9:
-            pass
-        else:
-            self.x_pos += 1
-            self.table[self.x_pos][self.y_pos].is_active = not grid.table[self.x_pos][self.y_pos].is_active
-            self.table[self.x_pos-1][self.y_pos].is_active = False
-
-    def move_left(self):
-        if self.x_pos == 0:
-            pass
-        else:
-            self.x_pos -= 1
-            self.table[self.x_pos][self.y_pos].is_active = not grid.table[self.x_pos][self.y_pos].is_active
-            self.table[self.x_pos+1][self.y_pos].is_active = False
-
-    def move_down(self):
-        if self.y_pos == 19:
-            pass
-        else:
-            self.y_pos += 1
-            self.table[self.x_pos][self.y_pos].is_active = True
-            self.table[self.x_pos][self.y_pos-1].is_active = False
+from grid import Grid
 
 def update():
     for row in grid.table:
@@ -53,14 +8,13 @@ def update():
                 pygame.draw.rect(screen, (0, 0, 0), square.rect)
     pygame.display.update()
 
-
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((400, 600))
 
     grid = Grid(10, 20)
 
-    pygame.time.set_timer(pygame.USEREVENT+1, 1000)
+    pygame.time.set_timer(pygame.USEREVENT+1, 100)
     running = True
     while running:
         screen.fill((255, 255, 255))
@@ -72,7 +26,8 @@ if __name__ == '__main__':
                 elif keys[pygame.K_LEFT]:
                     grid.move_left()
             if event.type == pygame.USEREVENT+1:
-                grid.move_down() 
+                grid.move_down()
+                
             if event.type == pygame.QUIT:
                 running = False
                 break
